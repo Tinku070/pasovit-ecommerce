fefb9aa2b367c8a7f01da6456581e85a70238aa3
Gowtham <bonaboinagowtham@gmail.com>
2025-12-01 21:51:10 +0530

    Fix api.js default export & correct API base URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://pasovit-ecommerce-eojr.onrender.com/api",
+  baseURL: "https://pasovit-ecommerce.onrender.com/api", // will update AFTER backend deploy
   withCredentials: true,
 });
 
