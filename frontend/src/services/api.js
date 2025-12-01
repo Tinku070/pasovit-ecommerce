1fded27ad8f696f69921802257c6870737548bc1
Gowtham <bonaboinagowtham@gmail.com>
2025-12-01 16:20:35 +0530

    Fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pasovit-ecommerce-eojr.onrender.com/api",
+  baseURL: "https://pasovit-ecommerce-eojr.onrender.com",
   withCredentials: true,
 });
 
