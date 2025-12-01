d729ac57eb77a9c07ad5bcce62e9d3e1f52602c2
Gowtham <bonaboinagowtham@gmail.com>
2025-12-01 21:37:03 +0530

    Fix api.js default export & correct API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,30 @@
-import axios from "axios";
+import API from "../api/axios";
 
-const api = axios.create({
-  baseURL: "https://pasovit-ecommerce-eojr.onrender.com",
-  withCredentials: true,
-});
+// Product APIs
+export const getProducts = () => API.get("/products");
+export const getProductById = (id) => API.get(`/products/${id}`);
 
-export default api;
+// Auth APIs
+export const login = (data) => API.post("/auth/login", data);
+export const registerUser = (data) => API.post("/auth/register", data);
+export const getMe = () => API.get("/auth/me");
+export const logoutUser = () => API.post("/auth/logout");
+
+// Cart APIs
+export const getCart = () => API.get("/cart");
+export const addToCart = (data) => API.post("/cart", data);
+export const removeFromCart = (productId) =>
+  API.delete(`/cart/${productId}`);
+
+// Default export (this FIXES your build)
+export default {
+  getProducts,
+  getProductById,
+  login,
+  registerUser,
+  getMe,
+  logoutUser,
+  getCart,
+  addToCart,
+  removeFromCart,
+};
